c49b895a341d7c958d1cdcb7a78b72e965a2aeb2
intrigun <scipiont@gmail.com>
2024-06-07 09:43:36 +0300

    0.0.8

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,3 +1,21 @@
+# from django import forms
+# from .models import Client, Product, Order
+
+# class ClientForm(forms.ModelForm):
+#     class Meta:
+#         model = Client
+#         fields = ['name', 'email', 'phone_number', 'address', 'registration_date']
+
+# class ProductForm(forms.ModelForm):
+#     class Meta:
+#         model = Product
+#         fields = ['name', 'description', 'price', 'quantity', 'date_added']
+
+# class OrderForm(forms.ModelForm):
+#     class Meta:
+#         model = Order
+#         fields = ['client', 'products', 'total_amount', 'order_date']
+
 from django import forms
 from .models import Client, Product, Order
 
@@ -5,13 +23,33 @@ class ClientForm(forms.ModelForm):
     class Meta:
         model = Client
         fields = ['name', 'email', 'phone_number', 'address', 'registration_date']
+        labels = {
+            'name': 'Имя клиента',
+            'email': 'Электронная почта клиента',
+            'phone_number': 'Номер телефона клиента',
+            'address': 'Адрес клиента',
+            'registration_date': 'Дата регистрации клиента',
+        }
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'description', 'price', 'quantity', 'date_added']
+        labels = {
+            'name': 'Название товара',
+            'description': 'Описание товара',
+            'price': 'Цена товара',
+            'quantity': 'Количество товара',
+            'date_added': 'Дата добавления товара',
+        }
 
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['client', 'products', 'total_amount', 'order_date']
+        labels = {
+            'client': 'Клиент',
+            'products': 'Товары',
+            'total_amount': 'Общая сумма заказа',
+            'order_date': 'Дата оформления заказа',
+        }
