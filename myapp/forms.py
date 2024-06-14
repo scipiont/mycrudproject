410dd79cac021ad2cd55c9f3a598a152ee7f6018
intrigun <scipiont@gmail.com>
2024-06-14 12:10:29 +0300

    хранения фотографии продукта 0.0.18

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,3 +1,5 @@
+
+
 # from django import forms
 # from .models import Client, Product, Order
 
@@ -5,16 +7,36 @@
 #     class Meta:
 #         model = Client
 #         fields = ['name', 'email', 'phone_number', 'address', 'registration_date']
+#         labels = {
+#             'name': 'Имя клиента',
+#             'email': 'Электронная почта клиента',
+#             'phone_number': 'Номер телефона клиента',
+#             'address': 'Адрес клиента',
+#             'registration_date': 'Дата регистрации клиента',
+#         }
 
 # class ProductForm(forms.ModelForm):
 #     class Meta:
 #         model = Product
 #         fields = ['name', 'description', 'price', 'quantity', 'date_added']
+#         labels = {
+#             'name': 'Название товара',
+#             'description': 'Описание товара',
+#             'price': 'Цена товара',
+#             'quantity': 'Количество товара',
+#             'date_added': 'Дата добавления товара',
+#         }
 
 # class OrderForm(forms.ModelForm):
 #     class Meta:
 #         model = Order
 #         fields = ['client', 'products', 'total_amount', 'order_date']
+#         labels = {
+#             'client': 'Клиент',
+#             'products': 'Товары',
+#             'total_amount': 'Количество заказа',
+#             'order_date': 'Дата оформления заказа',
+#         }
 
 from django import forms
 from .models import Client, Product, Order
@@ -34,13 +56,14 @@ class ClientForm(forms.ModelForm):
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['name', 'description', 'price', 'quantity', 'date_added']
+        fields = ['name', 'description', 'price', 'quantity', 'date_added', 'photo']
         labels = {
             'name': 'Название товара',
             'description': 'Описание товара',
             'price': 'Цена товара',
             'quantity': 'Количество товара',
             'date_added': 'Дата добавления товара',
+            'photo': 'Фотография товара',
         }
 
 class OrderForm(forms.ModelForm):
@@ -50,6 +73,6 @@ class OrderForm(forms.ModelForm):
         labels = {
             'client': 'Клиент',
             'products': 'Товары',
-            'total_amount': 'Количество заказа',
+            'total_amount': 'Общая сумма заказа',
             'order_date': 'Дата оформления заказа',
         }
