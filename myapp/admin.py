cf20a271c6564c637aad34ef66b444ee18d23bc2
intrigun <scipiont@gmail.com>
2024-06-18 15:40:13 +0300

    Добавлены миксины для выгрузки pdf, csv 0.0.30

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,125 +1,142 @@
+# from django.contrib import admin
+# from django.http import HttpResponse
+# from .models import Client, Product, Order
+# import csv
+# from reportlab.lib.pagesizes import letter, landscape
+# from reportlab.pdfgen import canvas
+# from reportlab.pdfbase.ttfonts import TTFont
+# from reportlab.pdfbase import pdfmetrics
+# from reportlab.lib.utils import simpleSplit
+
+# # Регистрация шрифта DejaVuSans
+# pdfmetrics.registerFont(TTFont('DejaVuSans', 'DejaVuSans.ttf'))
+
+# # Действия для выгрузки данных в CSV
+# def export_clients_csv(modeladmin, request, queryset):
+#     response = HttpResponse(content_type='text/csv')
+#     response['Content-Disposition'] = 'attachment; filename=clients.csv'
+#     response.write('\ufeff'.encode('utf8'))  
+#     writer = csv.writer(response)
+#     writer.writerow(['ID', 'Имя', 'Электронная почта', 'Номер телефона', 'Адрес', 'Дата регистрации'])
+#     for client in queryset:
+#         writer.writerow([client.id, client.name, client.email, client.phone_number, client.address, client.registration_date])
+#     return response
+
+# export_clients_csv.short_description = 'Экспорт в CSV'
+
+# def export_products_csv(modeladmin, request, queryset):
+#     response = HttpResponse(content_type='text/csv')
+#     response['Content-Disposition'] = 'attachment; filename=products.csv'
+#     response.write('\ufeff'.encode('utf8'))  
+#     writer = csv.writer(response)
+#     writer.writerow(['ID', 'Название', 'Описание', 'Цена', 'Количество', 'Дата добавления'])
+#     for product in queryset:
+#         writer.writerow([product.id, product.name, product.description, product.price, product.quantity, product.date_added])
+#     return response
+
+# export_products_csv.short_description = 'Экспорт в CSV'
+
+# def export_orders_csv(modeladmin, request, queryset):
+#     response = HttpResponse(content_type='text/csv')
+#     response['Content-Disposition'] = 'attachment; filename=orders.csv'
+#     response.write('\ufeff'.encode('utf8'))  
+#     writer = csv.writer(response)
+#     writer.writerow(['ID', 'Клиент', 'Общая сумма', 'Дата заказа'])
+#     for order in queryset:
+#         writer.writerow([order.id, order.client.name, order.total_amount, order.order_date])
+#     return response
+
+# export_orders_csv.short_description = 'Экспорт в CSV'
+
+# # Функция для добавления текста с переносом строк
+# def draw_wrapped_text(canvas, text, x, y, max_width):
+#     lines = simpleSplit(text, 'DejaVuSans', 12, max_width)
+#     for line in lines:
+#         canvas.drawString(x, y, line)
+#         y -= 14  
+#     return y
+
+# # Действия для выгрузки данных в PDF
+# def export_clients_pdf(modeladmin, request, queryset):
+#     response = HttpResponse(content_type='application/pdf')
+#     response['Content-Disposition'] = 'attachment; filename=clients.pdf'
+#     p = canvas.Canvas(response, pagesize=landscape(letter))
+#     p.setFont('DejaVuSans', 12)
+#     y = 550
+#     p.drawString(10, y, "Клиенты")
+#     y -= 20
+#     for client in queryset:
+#         text = f"ID: {client.id}, Имя: {client.name}, Электронная почта: {client.email}, Телефон: {client.phone_number}, Адрес: {client.address}, Дата регистрации: {client.registration_date}"
+#         y = draw_wrapped_text(p, text, 10, y, 750)
+#         y -= 10
+#     p.showPage()
+#     p.save()
+#     return response
+
+# export_clients_pdf.short_description = 'Экспорт в PDF'
+
+# def export_products_pdf(modeladmin, request, queryset):
+#     response = HttpResponse(content_type='application/pdf')
+#     response['Content-Disposition'] = 'attachment; filename=products.pdf'
+#     p = canvas.Canvas(response, pagesize=landscape(letter))
+#     p.setFont('DejaVuSans', 12)
+#     y = 550
+#     p.drawString(10, y, "Товары")
+#     y -= 20
+#     for product in queryset:
+#         text = f"ID: {product.id}, Название: {product.name}, Описание: {product.description}, Цена: {product.price}, Количество: {product.quantity}, Дата добавления: {product.date_added}"
+#         y = draw_wrapped_text(p, text, 10, y, 750)
+#         y -= 10
+#     p.showPage()
+#     p.save()
+#     return response
+
+# export_products_pdf.short_description = 'Экспорт в PDF'
+
+# def export_orders_pdf(modeladmin, request, queryset):
+#     response = HttpResponse(content_type='application/pdf')
+#     response['Content-Disposition'] = 'attachment; filename=orders.pdf'
+#     p = canvas.Canvas(response, pagesize=landscape(letter))
+#     p.setFont('DejaVuSans', 12)
+#     y = 550
+#     p.drawString(10, y, "Заказы")
+#     y -= 20
+#     for order in queryset:
+#         text = f"ID: {order.id}, Клиент: {order.client.name}, Общая сумма: {order.total_amount}, Дата заказа: {order.order_date}"
+#         y = draw_wrapped_text(p, text, 10, y, 750)
+#         y -= 10
+#     p.showPage()
+#     p.save()
+#     return response
+
+# export_orders_pdf.short_description = 'Экспорт в PDF'
+
+# # Регистрация моделей и добавление действий
+# class ClientAdmin(admin.ModelAdmin):
+#     actions = [export_clients_csv, export_clients_pdf]
+
+# class ProductAdmin(admin.ModelAdmin):
+#     actions = [export_products_csv, export_products_pdf]
+
+# class OrderAdmin(admin.ModelAdmin):
+#     actions = [export_orders_csv, export_orders_pdf]
+
+# admin.site.register(Client, ClientAdmin)
+# admin.site.register(Product, ProductAdmin)
+# admin.site.register(Order, OrderAdmin)
+
 from django.contrib import admin
-from django.http import HttpResponse
 from .models import Client, Product, Order
-import csv
-from reportlab.lib.pagesizes import letter, landscape
-from reportlab.pdfgen import canvas
-from reportlab.pdfbase.ttfonts import TTFont
-from reportlab.pdfbase import pdfmetrics
-from reportlab.lib.utils import simpleSplit
+from .admin_mixins import ExportCsvMixin, ExportPdfMixin
 
-# Регистрация шрифта DejaVuSans
-pdfmetrics.registerFont(TTFont('DejaVuSans', 'DejaVuSans.ttf'))
+class ClientAdmin(admin.ModelAdmin, ExportCsvMixin, ExportPdfMixin):
+    actions = ["export_as_csv", "export_as_pdf"]
 
-# Действия для выгрузки данных в CSV
-def export_clients_csv(modeladmin, request, queryset):
-    response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename=clients.csv'
-    response.write('\ufeff'.encode('utf8'))  
-    writer = csv.writer(response)
-    writer.writerow(['ID', 'Имя', 'Электронная почта', 'Номер телефона', 'Адрес', 'Дата регистрации'])
-    for client in queryset:
-        writer.writerow([client.id, client.name, client.email, client.phone_number, client.address, client.registration_date])
-    return response
+class ProductAdmin(admin.ModelAdmin, ExportCsvMixin, ExportPdfMixin):
+    actions = ["export_as_csv", "export_as_pdf"]
 
-export_clients_csv.short_description = 'Экспорт в CSV'
-
-def export_products_csv(modeladmin, request, queryset):
-    response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename=products.csv'
-    response.write('\ufeff'.encode('utf8'))  
-    writer = csv.writer(response)
-    writer.writerow(['ID', 'Название', 'Описание', 'Цена', 'Количество', 'Дата добавления'])
-    for product in queryset:
-        writer.writerow([product.id, product.name, product.description, product.price, product.quantity, product.date_added])
-    return response
-
-export_products_csv.short_description = 'Экспорт в CSV'
-
-def export_orders_csv(modeladmin, request, queryset):
-    response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename=orders.csv'
-    response.write('\ufeff'.encode('utf8'))  
-    writer = csv.writer(response)
-    writer.writerow(['ID', 'Клиент', 'Общая сумма', 'Дата заказа'])
-    for order in queryset:
-        writer.writerow([order.id, order.client.name, order.total_amount, order.order_date])
-    return response
-
-export_orders_csv.short_description = 'Экспорт в CSV'
-
-# Функция для добавления текста с переносом строк
-def draw_wrapped_text(canvas, text, x, y, max_width):
-    lines = simpleSplit(text, 'DejaVuSans', 12, max_width)
-    for line in lines:
-        canvas.drawString(x, y, line)
-        y -= 14  
-    return y
-
-# Действия для выгрузки данных в PDF
-def export_clients_pdf(modeladmin, request, queryset):
-    response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename=clients.pdf'
-    p = canvas.Canvas(response, pagesize=landscape(letter))
-    p.setFont('DejaVuSans', 12)
-    y = 550
-    p.drawString(10, y, "Клиенты")
-    y -= 20
-    for client in queryset:
-        text = f"ID: {client.id}, Имя: {client.name}, Электронная почта: {client.email}, Телефон: {client.phone_number}, Адрес: {client.address}, Дата регистрации: {client.registration_date}"
-        y = draw_wrapped_text(p, text, 10, y, 750)
-        y -= 10
-    p.showPage()
-    p.save()
-    return response
-
-export_clients_pdf.short_description = 'Экспорт в PDF'
-
-def export_products_pdf(modeladmin, request, queryset):
-    response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename=products.pdf'
-    p = canvas.Canvas(response, pagesize=landscape(letter))
-    p.setFont('DejaVuSans', 12)
-    y = 550
-    p.drawString(10, y, "Товары")
-    y -= 20
-    for product in queryset:
-        text = f"ID: {product.id}, Название: {product.name}, Описание: {product.description}, Цена: {product.price}, Количество: {product.quantity}, Дата добавления: {product.date_added}"
-        y = draw_wrapped_text(p, text, 10, y, 750)
-        y -= 10
-    p.showPage()
-    p.save()
-    return response
-
-export_products_pdf.short_description = 'Экспорт в PDF'
-
-def export_orders_pdf(modeladmin, request, queryset):
-    response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename=orders.pdf'
-    p = canvas.Canvas(response, pagesize=landscape(letter))
-    p.setFont('DejaVuSans', 12)
-    y = 550
-    p.drawString(10, y, "Заказы")
-    y -= 20
-    for order in queryset:
-        text = f"ID: {order.id}, Клиент: {order.client.name}, Общая сумма: {order.total_amount}, Дата заказа: {order.order_date}"
-        y = draw_wrapped_text(p, text, 10, y, 750)
-        y -= 10
-    p.showPage()
-    p.save()
-    return response
-
-export_orders_pdf.short_description = 'Экспорт в PDF'
-
-# Регистрация моделей и добавление действий
-class ClientAdmin(admin.ModelAdmin):
-    actions = [export_clients_csv, export_clients_pdf]
-
-class ProductAdmin(admin.ModelAdmin):
-    actions = [export_products_csv, export_products_pdf]
-
-class OrderAdmin(admin.ModelAdmin):
-    actions = [export_orders_csv, export_orders_pdf]
+class OrderAdmin(admin.ModelAdmin, ExportCsvMixin, ExportPdfMixin):
+    actions = ["export_as_csv", "export_as_pdf"]
 
 admin.site.register(Client, ClientAdmin)
 admin.site.register(Product, ProductAdmin)
---
+++ myapp/admin_mixins.py
@@ -0,0 +1,61 @@
+import csv
+from django.http import HttpResponse
+from reportlab.lib.pagesizes import letter, landscape
+from reportlab.pdfgen import canvas
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfbase import pdfmetrics
+from reportlab.lib.utils import simpleSplit
+from django.contrib.staticfiles import finders
+
+# Найти путь к шрифту DejaVuSans.ttf
+font_path = finders.find('fonts/DejaVuSans.ttf')
+if font_path:
+    pdfmetrics.registerFont(TTFont('DejaVuSans', font_path))
+else:
+    raise FileNotFoundError("Шрифт DejaVuSans.ttf не найден в папке static/fonts")
+
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = f'attachment; filename={meta}.csv'
+        response.write('\ufeff'.encode('utf8'))  # Добавляем BOM для корректного отображения в Excel
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Экспорт в CSV"
+
+class ExportPdfMixin:
+    def export_as_pdf(self, request, queryset):
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = f'attachment; filename={self.model._meta}.pdf'
+        p = canvas.Canvas(response, pagesize=landscape(letter))
+        p.setFont('DejaVuSans', 12)
+        y = 550
+        p.drawString(10, y, self.model._meta.verbose_name_plural.capitalize())
+        y -= 20
+
+        for obj in queryset:
+            text = ", ".join([f"{field.verbose_name}: {getattr(obj, field.name)}" for field in self.model._meta.fields])
+            y = self.draw_wrapped_text(p, text, 10, y, 750)
+            y -= 10
+
+        p.showPage()
+        p.save()
+        return response
+
+    def draw_wrapped_text(self, canvas, text, x, y, max_width):
+        lines = simpleSplit(text, 'DejaVuSans', 12, max_width)
+        for line in lines:
+            canvas.drawString(x, y, line)
+            y -= 14  # Высота строки
+        return y
+
+    export_as_pdf.short_description = "Экспорт в PDF"
